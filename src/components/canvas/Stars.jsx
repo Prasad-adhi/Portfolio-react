717e9e5487b0460446c7c8c8a72d72898ff8d438
Prasad-adhi <prasad.adhi08@gmail.com>
2025-06-05 20:04:55 -0400

    Completed Hero.Jsx

---
+++ src/components/canvas/Stars.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Canvas, useFrame} from "@react-three/fiber";
+import {Points, PointMaterial, Preload} from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
+import styled from "styled-components";
+
+
+const Stars = () => {
+  return (
+    <div>Stars</div>
+  )
+}
+
+export default Stars
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,123 +2,128 @@ import React from 'react';
 import styled from "styled-components";
 import { Bio } from "../../data/constants";
 import Typewriter from "typewriter-effect";
-import HeroImg from "../../images/PrasadAdhiyaman.png"
+import HeroImg from "../../images/20230928,565.jpg"
+import HeroBgAnimation from "../HeroBgAnimation";
+import {Tilt} from "react-tilt";
+import {motion} from "framer-motion";
+import {headContainerAnimation, headContentAnimation, headTextAnimation} from "../../utils/motion"
 
 const HeroContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    position: relative;
-    padding: 80px 30px;
-    z-index: 1;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding: 80px 30px;
+  z-index: 1;
 
-    @media (max-width: 960 px){
-        padding: 66px 16px;
-    }
-    
-    @media (max-width: 640 px){
-        padding: 32px 16px;
-    }
-    
-    clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95%, 0 100%);
+  @media (max-width: 960px) {
+    padding: 66px 16px;
+  }
+
+  @media (max-width: 640px) {
+    padding: 32px 16px;
+  }
+
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95%, 0 100%);
 `;
-
 const HeroInnerContainer = styled.div`
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    max-width: 1100px;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1100px;
 
-    @media (max-width: 960px) {
-        flex-direction: column;
-    }
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
 `;
 const HeroLeftContainer = styled.div`
-    width: 100%;
-    order: 1;
-    @media (max-width: 960px){
-        order: 2;
-        margin-bottom: 30px;
-        display: flex;
-        gap: 6px;
-        flex-direction: column;
-        align-items: center;
-    }
+  width: 100%;
+  order: 1;
+  @media (max-width: 960px) {
+    order: 2;
+    margin-bottom: 30px;
+    display: flex;
+    gap: 6px;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 const HeroRightContainer = styled.div`
-    width: 100%;
-    order: 2;
-    @media (max-width: 960px){
-        order: 1;
-        margin-bottom: 30px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-contents: center;
-        margin-bottom: 80px;
-    }
+  width: 100%;
+  order: 2;
+  display: flex;
+  justify-content: end;
+  @media (max-width: 960px) {
+    order: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-contents: center;
+    margin-bottom: 80px;
+  }
 
-    @media (max-width: 640px) {
-        margin-bottom: 30px;
-    }
+  @media (max-width: 640px) {
+    margin-bottom: 30px;
+  }
 `;
 
 const Title = styled.div`
-    font-weight: 700;
-    font-size: 50px;
-    color: ${({theme}) => theme.text_primary};
-    line-height: 68px;
+  font-weight: 700;
+  font-size: 50px;
+  color: ${({ theme }) => theme.text_primary};
+  line-height: 68px;
 
-    @media (max-width: 960px) {
-        text-align: center;
-    }
-    
-    @media (max-width: 960px) {
-        font-size: 40px;
-        line-height: 48px;
-        margin-bottom: 8px;
-    }
+  @media (max-width: 960px) {
+    text-align: center;
+  }
+
+  @media (max-width: 960px) {
+    font-size: 40px;
+    line-height: 48px;
+    margin-bottom: 8px;
+  }
 `;
 
 const TextLoop = styled.div`
-    font-weight: 600;
-    font-size: 32px;
-    display: flex;
-    gap: 12px;
-    color: ${({theme}) => theme.text_primary};
-    line-height: 68px;
+  font-weight: 600;
+  font-size: 32px;
+  display: flex;
+  gap: 12px;
+  color: ${({ theme }) => theme.text_primary};
+  line-height: 68px;
 
-    @media (max-width: 960px) {
-        text-align: center;
-    }
-    
-    @media (max-width: 960px) {
-        font-size: 22px;
-        line-height: 48px;
-        margin-bottom: 16px;
-    }
+  @media (max-width: 960px) {
+    text-align: center;
+  }
+
+  @media (max-width: 900px) {
+    font-size: 22px;
+    line-height: 48px;
+    margin-bottom: 16px;
+    text-align: center;
+  }
 `;
 
 const Span = styled.div`
-    cursor: pointer;
-    color: ${({theme}) => theme.primary};
+  cursor: pointer;
+  color: ${({ theme }) => theme.primary};
 `;
 
 const SubTitle = styled.div`
-    font-size: 20px;
-    line-height: 32px;
-    margin-bottom: 42px;
-    color: ${({theme}) => theme.text_primary + 95};
+  font-size: 20px;
+  line-height: 32px;
+  margin-bottom: 42px;
+  color: ${({ theme }) => theme.text_primary + 95};
 
-    @media (max-width: 960px) {
-        text-align: center;
-    }
-    
-    @media (max-width: 960px) {
-        font-size: 16px;
-        line-height: 32px;
-    }
+  @media (max-width: 960px) {
+    text-align: center;
+  }
+
+  @media (max-width: 960px) {
+    font-size: 16px;
+    line-height: 32px;
+  }
 `;
 
 const ResumeButton = styled.a`
@@ -169,49 +174,84 @@ const ResumeButton = styled.a`
 `;
 
 const Img = styled.img`
-    border-radius: 50%;
-    width: 50%;
-    height: 75%;
-    max-width: 500px;
-    max-height: 750px;
-    border: 2px solid ${({theme}) => theme.primary};
+  border-radius: 50%;
+  width: 100%;
+  height: 100%;
+  max-width: 400px;
+  max-height: 400px;
+  border: 2px solid ${({ theme }) => theme.primary};
 
-    @media (max-width: 900px) {
-        width: 100%;
-        height: 100%;
-        max-width: 250px;
-        max-height: 375px;
-    }
+  @media (max-width: 640px) {
+    max-width: 280px;
+    max-height: 280px;
+  }
+`;
+
+const HeroBg = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 1360px;
+  overflow: hidden;
+  padding: 0 30px;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+
+  @media (max-width: 960px) {
+    justify-content: center;
+    padding: 0 0px;
+  }
 `;
 
 const Hero = () => {
   return (
     <div id="about">
         <HeroContainer>
-            <HeroInnerContainer>
-                <HeroLeftContainer>
-                    <Title>
-                        Hi, I am <br/> {Bio.name}
-                    </Title>
-                    <TextLoop>
-                        I am a 
-                        <Span>
-                            <Typewriter 
-                                options = {{
-                                    strings: Bio.roles,
-                                    autoStart: true,
-                                    loop: true
-                                }}
-                            />
-                        </Span>
-                    </TextLoop>
-                    <SubTitle>{Bio.description}</SubTitle>
-                    <ResumeButton>Check Resume</ResumeButton>
-                </HeroLeftContainer>
-                <HeroRightContainer>
-                    <Img src = {HeroImg} alt="Prasad Adhiyaman" />
-                </HeroRightContainer>
-            </HeroInnerContainer>
+            <HeroBg>
+                <HeroBgAnimation />
+            </HeroBg>
+            <motion.div {...headContainerAnimation}>
+                <HeroInnerContainer>
+                    <HeroLeftContainer>
+                        <motion.div {...headTextAnimation}>
+                            <Title>
+                                Hi, I am <br/> {Bio.name}
+                            </Title>
+                            <TextLoop>
+                                I am a 
+                                <Span>
+                                    <Typewriter 
+                                        options = {{
+                                            strings: Bio.roles,
+                                            autoStart: true,
+                                            loop: true
+                                        }}
+                                    />
+                                </Span>
+                            </TextLoop>
+                        </motion.div>
+                        
+                        <motion.div {...headContentAnimation}>
+                            <SubTitle>{Bio.description}</SubTitle>
+                        </motion.div>
+                        
+                        <ResumeButton>Check Resume</ResumeButton>
+                    </HeroLeftContainer>
+                    <HeroRightContainer>
+                        <motion.div {...headContentAnimation}>
+                            <Tilt><Img src = {HeroImg} alt="Prasad Adhiyaman" /></Tilt>
+                        </motion.div>
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+            </motion.div>
         </HeroContainer>
     </div>
   )
