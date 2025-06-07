894edc509126394f966d9b34ef4d37ecd7dd3587
Prasad-adhi <prasad.adhi08@gmail.com>
2025-06-07 19:04:07 -0400

    Education section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Router } from "react-router-dom";
 import Hero from "./components/sections/Hero";
 import Skills from "./components/sections/Skills";
 import Experience from "./components/sections/Experience";
+import Education from "./components/sections/Education";
+import StartCanvas from "./components/canvas/Stars";
 
 const Body = styled.div`
   background-color: ${({theme}) => theme.bg};
@@ -15,17 +17,41 @@ const Body = styled.div`
   position: relative;
 `;
 
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
 function App() {
-  return <ThemeProvider theme={darkTheme}>
-    <BrowserRouter>
-      <Navbar/>
-      <Body>
-        <Hero />
-        <Skills /> 
-        <Experience />
-      </Body>
-    </BrowserRouter>
-  </ThemeProvider>
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <StartCanvas />
+          <div>
+            <Hero />
+            <Wrapper>
+              <Skills />
+              <Experience />
+            </Wrapper>
+            <Education />
+          </div>
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
